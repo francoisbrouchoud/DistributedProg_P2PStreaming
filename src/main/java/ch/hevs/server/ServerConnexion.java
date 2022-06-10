76addb10e5a42f48eb49cf0eff46a2f2bdf1b162
celine.vialard <celine.vialard@gmail.com>
2022-06-10 15:26:47 +0200

    logging begin

--- src/main/java/ch/hevs/server/ServerConnexion.java
+++ src/main/java/ch/hevs/server/ServerConnexion.java
@@ -3,20 +3,53 @@ package ch.hevs.server;
 import ch.hevs.common.ClientInfo;
 import ch.hevs.common.ServerBase;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.logging.*;
 
 public class ServerConnexion extends ServerBase {
     ArrayBlockingQueue<ClientInfo> clients;
+    private static final Logger LOGGER = Logger.getLogger(ServerConnexion.class.getName());
+
     public ServerConnexion(ServerSocket socketServer) {
         super(socketServer);
         clients = new ArrayBlockingQueue<>(20);
+        Handler consoleHandler = null;
+        Handler fileHandler  = null;
+        Formatter simpleFormatter = null;
+        try{
+            //Creating consoleHandler and fileHandler
+            consoleHandler = new ConsoleHandler();
+            // chez moi C:\Users\celin\AppData\Local\Temp
+            fileHandler  = new FileHandler("%t/serverJavaSocket.log", true);
+            // Creating SimpleFormatter
+            simpleFormatter = new SimpleFormatter();
+            fileHandler.setFormatter(simpleFormatter);
+
+            //Assigning handlers to LOGGER object
+            LOGGER.addHandler(consoleHandler);
+            LOGGER.addHandler(fileHandler);
+
+            //Setting levels to handlers and LOGGER
+            consoleHandler.setLevel(Level.ALL);
+            fileHandler.setLevel(Level.ALL);
+            LOGGER.setLevel(Level.ALL);
+
+            LOGGER.config("Configuration done.");
+
+            //Console handler removed
+            LOGGER.removeHandler(consoleHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void acceptClient(Socket socket) {
         Thread t = new Thread(new ServerClientConnexion(socket, clients));
+        LOGGER.info("New Client connected");
         t.start();
     }
 }
